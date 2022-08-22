339a96b7afefd57f135b38825e8f49808bc8dfb6
sundhar-singh <90552685+sundhar-singh@users.noreply.github.com>
2022-08-22 22:57:38 +0530

    Add files via upload

---
+++ Config/auth.config.js
@@ -0,0 +1,7 @@
+/**
+ * This file contains the neccessary data for encryption
+ */
+
+ module.exports = {
+    secert : "my first Project"
+}
---
+++ Config/db.config.js
@@ -0,0 +1,7 @@
+/**
+ * This file contains the URL of the mongodb for connection
+ */
+
+ module.exports = {
+    DB_URL: "mongodb://localhost/project"
+}
